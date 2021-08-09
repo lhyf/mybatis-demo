9ad35eb9a78c67e18efe7e3b1f9f010f9090fef3
LHYF <lhyf@outlook.com>
2021-08-09 23:22:39 +0800

    添加TypeHandler测试代码

---
+++ mybatis-01-helloworld/src/main/java/org/lhyf/mybatis/handler/VarcharTypeHandler.java
@@ -0,0 +1,41 @@
+package org.lhyf.mybatis.handler;
+
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.MappedJdbcTypes;
+import org.apache.ibatis.type.MappedTypes;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * @author YF
+ * @date 2021/08/09 22:56
+ * @Description
+ */
+@MappedTypes(String.class)
+@MappedJdbcTypes(JdbcType.CHAR)
+public class VarcharTypeHandler extends BaseTypeHandler<String> {
+
+    @Override
+    public void setNonNullParameter(PreparedStatement ps, int i, String parameter, JdbcType jdbcType) throws SQLException {
+        ps.setString(i, parameter);
+    }
+
+    @Override
+    public String getNullableResult(ResultSet rs, String columnName) throws SQLException {
+        return rs.getString(columnName);
+    }
+
+    @Override
+    public String getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+        return rs.getString(columnIndex);
+    }
+
+    @Override
+    public String getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+        return cs.getString(columnIndex);
+    }
+}
